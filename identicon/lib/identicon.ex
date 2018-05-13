686aa380ff4a21aa0a0e64fbe896565b6e20372b
yangeric7 <yangeric7@gmail.com>
2018-05-12 22:47:47 -0500

    hashing a string -- |> standard

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,16 +3,13 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
+  def main(input) do
+    input
+    |> hash_string()
+  end
 
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_string(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
