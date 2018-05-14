bfbec37cae357a02505065120aacb5648a0340d4
yangeric7 <yangeric7@gmail.com>
2018-05-13 21:23:32 -0500

    added identicon image struct and pick color function with pattern matching

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,11 +5,20 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |> hash_string()
+    |> hash_string
+    |> pick_color
+
   end
 
   def hash_string(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r, g, b]
   end
 end
