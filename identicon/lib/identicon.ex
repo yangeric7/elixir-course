8ceb3e84b3b3959aa7673db78fbdd0ea836bc1a5
yangeric7 <yangeric7@gmail.com>
2018-05-15 23:25:05 -0500

    created a pixel map for putting into the image later

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -8,6 +8,8 @@ defmodule Identicon do
     |> hash_string
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
 
   end
 
@@ -43,5 +45,27 @@ defmodule Identicon do
 
     # [145, 46, 200, 46, 145] (new operatoe ++ which appends two lists)
     row  ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+     grid = Enum.filter grid, fn({code, _index}) -> #convention is that if you have a function inside a function, remove parens and then new line the function
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
